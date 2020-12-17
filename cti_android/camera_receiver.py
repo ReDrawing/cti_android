187fc1a5eb5aea739ee27cdee746b37754753eb4
EltonCN <43186596+EltonCN@users.noreply.github.com>
2020-12-17 18:49:16 -0300

    Melhorado performance do camera_receiver

--- cti_android/camera_receiver.py
+++ cti_android/camera_receiver.py
@@ -3,6 +3,7 @@ from urllib.error import URLError
 import json
 import rclpy
 from rclpy.node import Node
+from rclpy.callback_groups import ReentrantCallbackGroup
 import sys
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
@@ -23,49 +24,57 @@ class CameraReceiver(Node):
         self.declare_parameter("ip","192.168.2.101")
         self.declare_parameter("frame_id","cellphone_camera")
 
+        ip = self.get_parameter("ip").get_parameter_value().string_value
+
+        self.cap = cv.VideoCapture("http://"+ip+":8080/video")
+
+        self.callbackGroup = ReentrantCallbackGroup()
+
         timer_period = 0.016666  # segundos
-        self.timer = self.create_timer(timer_period, self.timerCallback)
+        self.timer = self.create_timer(timer_period, self.timerCallback)#, callback_group=self.callbackGroup)
 
     def timerCallback(self):
         '''!
             Callback do timer que recebe a imagem e publica
         '''
+        
+        ret, frame = self.cap.read()
 
-        ip = self.get_parameter("ip").get_parameter_value().string_value
         frameId = self.get_parameter("frame_id").get_parameter_value().string_value
 
-        url = "http://"+ip+":8080/shot.jpg"
-        try:
-            with urllib.request.urlopen(url) as req:
-                frame = np.array(bytearray(req.read()), dtype = np.uint8)
-                frame = cv.imdecode(frame, -1)
+        tempo1 = cv.getTickCount()
 
-                frameRGB = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        if(ret):
+
+            imgMsg = Image()
+
+            data = frame.flatten().tolist()
+
+            imgMsg._data = data
+
+            imgMsg.height = frame.shape[0]
+            imgMsg.width = frame.shape[1]
+
+            imgMsg.encoding = "8UC3"
+            imgMsg.is_bigendian = 0
+            imgMsg.step = imgMsg.width*3
+
+            time = self.get_clock().now().to_msg()
+            imgMsg.header.stamp = time
             
-                bridge = CvBridge()
-                imgMsg = bridge.cv2_to_imgmsg(frameRGB)
-                compImgMsg = bridge.cv2_to_compressed_imgmsg(frameRGB)
-
-                time = self.get_clock().now().to_msg()
-                imgMsg.header.stamp = time
-                compImgMsg.header.stamp = time
-                
-                imgMsg.header.frame_id = frameId
-                compImgMsg.header.frame_id = frameId
+            imgMsg.header.frame_id = frameId
             
+            self.pubImg.publish(imgMsg)
 
-                self.pubImg.publish(imgMsg)
-                self.pubImgCompress.publish(compImgMsg)
+            #compImgMsg = bridge.cv2_to_compressed_imgmsg(frame)
+            #compImgMsg.header.stamp = time
+            #compImgMsg.header.frame_id = frameId
+            #self.pubImgCompress.publish(compImgMsg)
 
-                self.publishInfo(imgMsg.header)
+            self.publishInfo(imgMsg.header)
 
-        except URLError:
-            self.get_logger().warn("Nao foi possivel conectar. O endereco ip foi definido corretamente?")
-        except KeyError:
-            self.get_logger().warn("O celular esta conectado?")
-        except ConnectionResetError:
-            self.get_logger().warn("Conexao cancelada. O celular foi desconectado?")
-            
+        tempo2 = cv.getTickCount()
+
 
     def publishInfo(self, header):
         '''!
@@ -79,9 +88,15 @@ def main(args=None):
 
     node = CameraReceiver()
 
+    executor = rclpy.executors.MultiThreadedExecutor()
+    executor.add_node(node)
+
+    #executor.spin()
+
     rclpy.spin(node)
-    
+
     node.destroy_node()
+    executor.shutdown()
     rclpy.shutdown()
 
 
